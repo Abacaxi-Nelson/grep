4c89aab5ff806f30e12fe8ab56f8b779aa7c1e58
Nunes Nelson <nunes.nelson4@gmail.com>
2020-09-01 22:52:54 +0200

    rewrite with module

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,17 @@
 use std::env;
-use std::fs;
-use std::error::Error;
 use std::process;
+
+mod lib;
+use lib::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap();
 
-    if let Err(e) = run(config) {
+    if let Err(e) = lib::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-} 
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
